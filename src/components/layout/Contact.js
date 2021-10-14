be61a4c12f480e60840ad2cff0a1758eab71dbe3
Madhav Reddy Chintapalli <chintapallimadhav@gmail.com>
2021-10-14 01:06:59 -0400

    second commit

--- src/components/layout/Contact.js
+++ src/components/layout/Contact.js
@@ -5,21 +5,27 @@ function Contact() {
     <div className='footer-container-fluid colored-section' id='contact'>
       <h2>Contact</h2> <br />
       <div className='row'>
-        <div className='col-lg-3'>
+        <div className='col-lg-6'>
           <h4>Email</h4>
+          <i class='bi bi-envelope'></i>
           <p>chintapallimadhav@gmail.com</p>
-        </div>
-        <div className='col-lg-3'>
           <h4>Phone</h4>
+          <i class='bi bi-telephone'></i>
           <p>+1 6475458423</p>
-        </div>
-        <div className='col-lg-3'>
           <h4>Address</h4>
+          <i class='bi bi-geo-alt'></i>
           <p>12 Lynmont Road, Etobicoke, ON M9V 3W8</p>
         </div>
-        <div className='col-lg-3'>
+        <div className='col-lg-6'>
           <h4>Linkedin</h4>
+          <i class='bi bi-linkedin'></i>
           <p>linkedin.com/in/madhavreddychintapalli/</p>
+          <h4>Github</h4>
+          <i class='bi bi-github'></i>
+          <p>github.com/MadhavReddyChintapalli</p>
+          <h4>Portfolio</h4>
+          <i class='bi bi-briefcase'></i>
+          <p>https://fast-plateau-72751.herokuapp.com/</p>
         </div>
       </div>
       <br />
